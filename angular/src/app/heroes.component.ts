cc848581a1850b0995139372eec17ad73a40bf55
Robert Alonso <robert.alonso.ac@gmail.com>
2017-05-31 08:59:08 -0500

    Routing and styling

--- angular/src/app/heroes.component.ts
+++ angular/src/app/heroes.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
 import { HeroService } from './hero.service';
 import { Hero } from './hero.model';
 
 @Component({
   selector: 'my-heroes',
-  templateUrl: 'heroes.component.html',
+  templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -24,5 +29,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
